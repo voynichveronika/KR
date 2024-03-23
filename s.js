bb8e53dc159d2f19f6c13a396ec7a9c9fdd5a8a0
voynichveronika <veronika47333@gmail.com>
2024-03-23 23:20:09 +0300

    5 commit

---
+++ s.js
@@ -0,0 +1,24 @@
+function vivod()
+{               
+    var name  = document.getElementById('text1').value;
+    var phone = document.getElementById('text2').value;
+    var datez = document.getElementById('text3').value;
+    var datev = document.getElementById('text4').value;
+    var kol = document.getElementById('text5').value;
+    var inf = document.getElementById('text6').value;
+
+    if (name == "")
+    {
+        document.querySelector('.v1').innerHTML = "Поля отмеченные * обязательны для заполнения";
+    }
+    else
+    {
+        if (phone == "")
+        {
+            document.querySelector('.v2').innerHTML = "Поля отмеченные * обязательны для заполнения";  
+        }
+        else
+        {                                                        
+        }                    
+    }
+}
